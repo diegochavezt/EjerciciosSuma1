79c2f0c6babb060d74de62f50786a7c97f5692e0
Diego <diegoandre.chavezt@gmail.com>
2022-11-17 17:11:16 -0500

    EjerciciosSuma1

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String[] args) {
+        suma (10, 30, 60);
+    }
+
+    private static void suma(int a, int b, int c) {
+        int resultado;
+        resultado = (a + b + c);
+        System.out.println(resultado);
+        
+    }
+}
